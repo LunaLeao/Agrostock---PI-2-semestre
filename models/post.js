c32770159bb02a06091a36ab66c8ef6a49db290c
NiklasRamon <166706900+NiklasRamon@users.noreply.github.com>
2024-11-22 13:14:32 -0300

    Add files via upload

--- models/post.js
+++ models/post.js
@@ -497,4 +497,4 @@ Colheita.hasMany(Venda, { foreignKey: 'colheitaId' });
 module.exports = {Usuario,TipoProduto,Colheita,Endereco,TipoInsumo,TipoRelatorio,Relatorios,CalculoLucro,Comprador,Cotacao,EstoqueColheita,EstoqueInsumo,Venda,Fornecedor,Insumo,Compra};
 
 //db.sequelize.sync ({force: true}) //mudar pra alter no lugar de force caso queira atualizar apenas
-db.sequelize.sync({ alter: true })
+//db.sequelize.sync({ alter: true })
